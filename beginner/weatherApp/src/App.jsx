c53179b257e63b51c9341eb78dd28a12bbc95a8c
amarphule <amardeepphule@gmail.com>
2024-04-13 20:58:07 +0530

    api-key inport for vite

--- beginner/weatherApp/src/App.jsx
+++ beginner/weatherApp/src/App.jsx
@@ -9,10 +9,12 @@ function App() {
   useEffect(() => {
     const fetchWeather = async () => {
       try {
+        const apiKey = import.meta.env.VITE_API_KEY;
         const response = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_API_KEY}`
+          `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`
         );
         const data = await response.json();
+        console.log(data);
         setWeatherData(data);
       } catch (error) {
         console.error("Error fetching weather data:", error);
