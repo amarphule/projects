c400e6f0387190ad26b1a554232b8ef984ab0212
amarphule <amardeepphule@gmail.com>
2024-05-26 11:50:30 +0530

    add tailwind, redux toolkit with config

--- intermediate/blogify/frontend/src/App.jsx
+++ intermediate/blogify/frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <h1>Blogging website</h1>;
+  return (
+    <div className="App">
+      <header className="bg-blue-600 text-white p-4">
+        <h1 className="text-center text-3xl">Blogging website</h1>
+      </header>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
